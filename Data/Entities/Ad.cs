c2e2cf9ccdbd1653b06d65214a58977c146dd97a
Karin Eurenius <93973477+Karre91@users.noreply.github.com>
2022-12-06 20:13:16 +0000

    DB setup with table.

---
+++ Data/Entities/Ad.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LingonberryStudio.Data.Entities
+{
+    public class Ad
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Category { get; set; }
+        public string Area { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ Data/Seed.cs
@@ -0,0 +1,45 @@
+﻿using LingonberryStudio.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LingonberryStudio.Data
+{
+    public class Seed
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new LingonberryDbContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<LingonberryDbContext>>()))
+            {
+
+                if (context.Ads.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Ads.AddRange(
+                    new Ad()
+                    {
+                        Category = "HaveStudio,",
+                        Area = "CF64 4NJ",
+                        Price = 100
+                    },
+                    new Ad()
+                    {
+                        Category = "SearchingForStudio,",
+                        Area = "CF64 5KL",
+                        Price = 200
+                    }                    
+                    );
+
+                if (context.Ads.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
