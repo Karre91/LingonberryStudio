185e42e8793ad428cc99e73ea1b72064950f03f4
Karin Eurenius <93973477+Karre91@users.noreply.github.com>
2022-12-07 13:51:33 +0000

    Update.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,22 +1,18 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-
-
-
+﻿
 const slideshowImages = document.querySelectorAll(".slideshow-img");
 
 const nextImageDelay = 1000;
 let currentImageCounter = 0;
 
-slideshowImages[currentImageCounter].style.display = "block";
+slideshowImages[currentImageCounter].style.display = "block";  //HELP
 
 setInterval(nextImage, nextImageDelay);
 
 function nextImage() {
     
-    slideshowImages[currentImageCounter].style.display = "block";
-    currentImageCounter = (currentImageCounter + 1) % slideshowImages.length;
-
+    slideshowImages[currentImageCounter].style.display = "none"; //HELP
+    currentImageCounter = (currentImageCounter + 1);
+    if (currentImageCounter == slideshowImages.length) {
+        currentImageCounter = 0;
+    }
 }
