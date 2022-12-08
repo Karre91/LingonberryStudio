1dd97021b6f3850115addfb0d05c8de7c09d43d7
Karin Eurenius <93973477+Karre91@users.noreply.github.com>
2022-12-08 09:38:45 +0000

    Update.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -5,15 +5,14 @@ if (slideshowImages.length != 0) {
     const nextImageDelay = 1000;
     let currentImageCounter = 0;
 
-    slideshowImages[currentImageCounter].style.display = "block";  //HELP
+    slideshowImages[currentImageCounter].style.display = "block";
 
     setInterval(nextImage, nextImageDelay);
 
     function nextImage() {
-
-        slideshowImages[currentImageCounter].style.display = "none"; //HELP
+        slideshowImages[currentImageCounter].style.display = "none";
         currentImageCounter = (currentImageCounter + 1) % slideshowImages.length;
-        slideshowImages[currentImageCounter].style.display = "block";  //HELP
+        slideshowImages[currentImageCounter].style.display = "block";
     }
 }
 
