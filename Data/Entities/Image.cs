c028f1880906577ae1681b4c0614c5f60ca04853
Karin Eurenius <93973477+Karre91@users.noreply.github.com>
2022-12-16 19:46:04 +0000

    Start.

---
+++ Data/Entities/Image.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LingonberryStudio.Data.Entities
+{
+    public class Image
+    {
+        [Key]
+        public int ImageID { get; set; }
+
+        public string? ImgUrl { get; set; }
+
+        [NotMapped]
+        public IFormFile? formFile { get; set; }
+    }
+}
