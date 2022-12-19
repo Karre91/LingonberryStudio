689f391d09e04372e52d20581562778b1d450679
josefinunefaldt <josefin6@hotmail.com>
2022-12-19 16:29:23 +0100

    Date validation is now working.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -17,6 +17,25 @@
         }
     }
 }
+
+$(function () {
+    var today = new Date();
+    var month = ('0' + (today.getMonth() + 1)).slice(-2);
+    var day = ('0' + today.getDate()).slice(-2);
+    var year = today.getFullYear();
+    var date = year + '-' + month + '-' + day;
+    $('[id*=from]').attr('min', date);
+});
+
+$(document).on("change", "#from", function () {
+    debugger
+    var date = $(this).val();
+
+    $('#to').attr('min', date);
+
+});
+
+
 $('#popupForm').load("/Adverts/Form");
 
 
