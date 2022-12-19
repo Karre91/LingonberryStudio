3575485d41b983ff93f80a4db54b862a96441618
josefinunefaldt <josefin6@hotmail.com>
2022-12-19 17:06:44 +0100

    Additional validation on date form.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -18,19 +18,16 @@
     }
 }
 
-$(function () {
-    var today = new Date();
-    var month = ('0' + (today.getMonth() + 1)).slice(-2);
-    var day = ('0' + today.getDate()).slice(-2);
-    var year = today.getFullYear();
-    var date = year + '-' + month + '-' + day;
-    $('[id*=from]').attr('min', date);
-});
-
 $(document).on("change", "#from", function () {
     debugger
     var date = $(this).val();
     $('#to').attr('min', date);
+});
+
+$(document).on("change", "#to", function () {
+    debugger
+    var date = $(this).val();
+    $('#from').attr('min', date);
 });
 
 
