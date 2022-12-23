cd8cd9cc434f45a931413e552bb3d9a05a340051
Karin Eurenius <93973477+Karre91@users.noreply.github.com>
2022-12-23 15:21:08 +0000

    Add migration.

---
+++ Migrations/20221223144522_1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LingonberryStudio.Migrations
+{
+    /// <inheritdoc />
+    public partial class _1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Amenities",
+                columns: table => new
+                {
+                    AmenityID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Parking = table.Column<bool>(type: "bit", nullable: false),
+                    AirCon = table.Column<bool>(type: "bit", nullable: false),
+                    Kitchen = table.Column<bool>(type: "bit", nullable: false),
+                    NaturalLight = table.Column<bool>(type: "bit", nullable: false),
+                    AcousticTreatment = table.Column<bool>(type: "bit", nullable: false),
+                    RunningWater = table.Column<bool>(type: "bit", nullable: false),
+                    Storage = table.Column<bool>(type: "bit", nullable: false),
+                    Other = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Amenities", x => x.AmenityID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Budgets",
+                columns: table => new
+                {
+                    BudgetId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MonthOrWeek = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Budgets", x => x.BudgetId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Days",
+                columns: table => new
+                {
+                    DayId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Monday = table.Column<bool>(type: "bit", nullable: false),
+                    Tuesday = table.Column<bool>(type: "bit", nullable: false),
+                    Wednesday = table.Column<bool>(type: "bit", nullable: false),
+                    Thursday = table.Column<bool>(type: "bit", nullable: false),
+                    Friday = table.Column<bool>(type: "bit", nullable: false),
+                    Saturday = table.Column<bool>(type: "bit", nullable: false),
+                    Sunday = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Days", x => x.DayId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Descriptions",
+                columns: table => new
+                {
+                    ImageID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ImgUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Desc = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Descriptions", x => x.ImageID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Measurements",
+                columns: table => new
+                {
+                    MeasurementID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FeetOrMeters = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Number = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Measurements", x => x.MeasurementID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DatesAndTimes",
+                columns: table => new
+                {
+                    DatesAndTimeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    OpeningTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ClosingTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DayID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DatesAndTimes", x => x.DatesAndTimeId);
+                    table.ForeignKey(
+                        name: "FK_DatesAndTimes_Days_DayID",
+                        column: x => x.DayID,
+                        principalTable: "Days",
+                        principalColumn: "DayId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Adverts",
+                columns: table => new
+                {
+                    AdvertId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TimeCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<int>(type: "int", nullable: true),
+                    OfferingLooking = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WorkspaceDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Area = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AmenityID = table.Column<int>(type: "int", nullable: false),
+                    BudgetId = table.Column<int>(type: "int", nullable: false),
+                    MeasurementID = table.Column<int>(type: "int", nullable: false),
+                    DatesAndTimeID = table.Column<int>(type: "int", nullable: false),
+                    Artist = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SocialMedia = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DescriptionID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Adverts", x => x.AdvertId);
+                    table.ForeignKey(
+                        name: "FK_Adverts_Amenities_AmenityID",
+                        column: x => x.AmenityID,
+                        principalTable: "Amenities",
+                        principalColumn: "AmenityID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Adverts_Budgets_BudgetId",
+                        column: x => x.BudgetId,
+                        principalTable: "Budgets",
+                        principalColumn: "BudgetId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Adverts_DatesAndTimes_DatesAndTimeID",
+                        column: x => x.DatesAndTimeID,
+                        principalTable: "DatesAndTimes",
+                        principalColumn: "DatesAndTimeId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Adverts_Descriptions_DescriptionID",
+                        column: x => x.DescriptionID,
+                        principalTable: "Descriptions",
+                        principalColumn: "ImageID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Adverts_Measurements_MeasurementID",
+                        column: x => x.MeasurementID,
+                        principalTable: "Measurements",
+                        principalColumn: "MeasurementID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Adverts_AmenityID",
+                table: "Adverts",
+                column: "AmenityID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Adverts_BudgetId",
+                table: "Adverts",
+                column: "BudgetId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Adverts_DatesAndTimeID",
+                table: "Adverts",
+                column: "DatesAndTimeID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Adverts_DescriptionID",
+                table: "Adverts",
+                column: "DescriptionID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Adverts_MeasurementID",
+                table: "Adverts",
+                column: "MeasurementID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DatesAndTimes_DayID",
+                table: "DatesAndTimes",
+                column: "DayID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Adverts");
+
+            migrationBuilder.DropTable(
+                name: "Amenities");
+
+            migrationBuilder.DropTable(
+                name: "Budgets");
+
+            migrationBuilder.DropTable(
+                name: "DatesAndTimes");
+
+            migrationBuilder.DropTable(
+                name: "Descriptions");
+
+            migrationBuilder.DropTable(
+                name: "Measurements");
+
+            migrationBuilder.DropTable(
+                name: "Days");
+        }
+    }
+}
