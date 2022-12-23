074bead036548791e64f0856d10197df6e9000e8
Karin Eurenius <93973477+Karre91@users.noreply.github.com>
2022-12-23 14:58:28 +0000

    Error page update.

--- Program.cs
+++ Program.cs
@@ -17,6 +17,8 @@ var app = builder.Build();
 //	Seed.Initialize(services);
 //}
 
+app.UseDeveloperExceptionPage();
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -24,6 +26,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
