fa1754bd049e481108c6de29d81ecfeeb347cada
Karin Eurenius <93973477+Karre91@users.noreply.github.com>
2023-01-11 09:02:56 +0000

    Fix.

--- Models/gmail.cs
+++ Models/gmail.cs
@@ -3,19 +3,18 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace LingonberryStudio.Models
 {
-	public class gmail
-	{
-		[AllowNull]
-		public string ?To { get; set; }
+    public class Gmail
+    {
+        [AllowNull]
+        public string? To { get; set; }
         [RegularExpression(@"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}", ErrorMessage = "Not a valid Email address")]
         public string From { get; set; }
-		public string Subject { get; set; }
-		
-		public string Body { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
 
         [RegularExpression(@"^[a-zA-Z''-'\s-]{1,60}$",
-	 ErrorMessage = "Only letters allowed")]
+        ErrorMessage = "Only letters allowed")]
         public string Name { get; set; }
 
-	}
+    }
 }
